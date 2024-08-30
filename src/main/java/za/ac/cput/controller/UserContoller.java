6aa2a38e90d7d416646910ffe08ffe090cf9c6b5
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-08-30 20:01:25 +0200

    Add files via upload

--- src/main/java/za/ac/cput/controller/UserContoller.java
+++ src/main/java/za/ac/cput/controller/UserContoller.java
@@ -1,7 +1,10 @@
 package za.ac.cput.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import za.ac.cput.domain.User;
 import za.ac.cput.service.UserService;
 
@@ -31,4 +34,21 @@ public class UserContoller {
         return userService.getAll();
     }
 
+    public ResponseEntity<?> addUserWithImage(@RequestPart User user, @RequestPart MultipartFile imageFile) {
+        try {
+            User savedUser = userService.addUserWithImage(user, imageFile);
+            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+//    @PostMapping("/addWithBase64Image")
+//    public ResponseEntity<?> addUserWithBase64Image(@RequestBody User user, @RequestParam String base64Image) {
+//        try {
+//            User savedUser = userService.addUserWithBase64Image(user, base64Image);
+//            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
