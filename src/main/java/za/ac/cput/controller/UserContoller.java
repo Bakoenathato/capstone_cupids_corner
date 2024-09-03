53cce1065a335076fa6afd942974735bdf3bbc3f
Braedon0000 <127891987+Braedon0000@users.noreply.github.com>
2024-09-03 22:48:34 +0200

    Add files via upload

--- src/main/java/za/ac/cput/controller/UserContoller.java
+++ src/main/java/za/ac/cput/controller/UserContoller.java
@@ -1,54 +1,53 @@
 package za.ac.cput.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import za.ac.cput.domain.User;
 import za.ac.cput.service.UserService;
+import za.ac.cput.util.LoginDTO;
+import za.ac.cput.util.LoginResponse;
 
 import java.util.List;
 
 @RestController
+//@CrossOrigin
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:5173")
+
 public class UserContoller {
     @Autowired
     private UserService userService;
-    @PostMapping("/create")
-    public User read(@RequestBody User user){
-        return userService.create(user);
-    }
-    @GetMapping("/read/{userId}")
 
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody User user) {
+        try {
+            User createdUser = userService.create(user);
+            return ResponseEntity.ok(createdUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/read/{userId}")
     public User read(@PathVariable long userId){
         return userService.read(userId);
     }
+
     @PostMapping("/update")
     public User update(@RequestBody User user){
         return userService.update(user);
     }
+
     @GetMapping("/getall")
     public List<User>getall(){
         return userService.getAll();
     }
 
-    public ResponseEntity<?> addUserWithImage(@RequestPart User user, @RequestPart MultipartFile imageFile) {
-        try {
-            User savedUser = userService.addUserWithImage(user, imageFile);
-            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO loginDTO){
+        LoginResponse loginResponse = userService.loginUser(loginDTO);
+        return ResponseEntity.ok(loginResponse);
     }
-//    @PostMapping("/addWithBase64Image")
-//    public ResponseEntity<?> addUserWithBase64Image(@RequestBody User user, @RequestParam String base64Image) {
-//        try {
-//            User savedUser = userService.addUserWithBase64Image(user, base64Image);
-//            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
+
 }
